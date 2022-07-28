9e46ea2419aef7a9ef6828c8fe91080ec0e9be55
Aron Bahram <aron.bahram@gmail.com>
2022-07-28 10:10:18 +0200

    Write few lines of documentation

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
+"""
+Use this package to define application-wide constants or other definitions.
+"""
 import os
 
+# Avoid using relative paths. Instead, whenever you create a path use this as the root for that path.
+# This will help evade relative path errors when a file or package gets moved.
 APP_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
